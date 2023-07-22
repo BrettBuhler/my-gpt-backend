53b01eb8eb7d5b61551194c8576897d24dc1cc7d
BrettBuhler <107363334+BrettBuhler@users.noreply.github.com>
2023-07-21 20:15:44 -0600

    Added build file and get(/) route to serve static file

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express ()
 app.use(express.json())
 const apiRoutes = require('./routes/apiRoutes.js')
+const mainRoute = require('./routes/mainRoute.js')
 const cors = require('cors')
 
 app.use(cors({
@@ -10,6 +11,8 @@ app.use(cors({
 }))
 
 app.use('/api', apiRoutes)
+app.use(mainRoute)
+app.use(express.static('public'))
 
 const port = process.env.PORT || 5000
 
